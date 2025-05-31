ea99ac2b91fe5dfe770e9016b9aa42e5930f392a
RaymondBrien <raytbrien@gmail.com>
2025-05-31 12:58:48 -0600

    Boilerplate test for further development

--- tests/test-keyboard-color.py
+++ tests/test-keyboard-color.py
@@ -1,48 +1,40 @@
-from Typing import Tuple
+from typing import Dict, Any, Generator
 import pytest
 import string
 
 from keyboard import RGB
 
-@pytest.fixture(scope=module)
+@pytest.fixture
 def color():
-    return RGB()
+    yield RGB()
 
-@pytest.fixture
-def valid_rgb_tuple() -> Tuple[int, int, int]:
-    return (50, 50, 50)
+# @pytest.fixture
+# def valid_rgb_dict() -> Generator[Dict[str, int], None, None]:
+#     yield {'r': 50, 'g': 50, 'b': 50}
 
-@pytest.fixture
-@pytest.paramterize("_tuple", "('(50)', '(50, 50)', '(500, 50, 50)', '("a", "b", "c")'")
-def bad_rgb_tuple() -> Tuple[Any]:
-    return _tuple
-
-
-def Display(rgb_tuple):
-    """
-    Display class requiers a valid rgb tuple.
-    Mock displaying a validated rgb tuple 
-    """
-    valid = Tuple[int, int, int]
-    if isinstance(rgb_tuple, valid):
-        return True
-
-    return False
+# # @pytest.fixture
+# # @pytest.paramterize("_tuple", "('(50)', '(50, 50)', '(500, 50, 50)', '("a", "b", "c")'")
+# # def bad_rgb_dict() -> Dict[str, int]:
+# #     yield _tuple
 
 
-# tests to write: 
-    # check display falls back gracefully if invalid rgb
-    # how does RGB class handle losing contact with display?
-    # how does display handle different tuple feed regularities? eg every 0.5 seconds, every 0.1s, every minute, series of repeats, does that break?
+# def Display(rgb_dict):
+#     """
+#     Display class requiers a valid rgb tuple.
+#     Mock displaying a validated rgb tuple
+#     """
+#     if isinstance(rgb_dict, dict):
+#         return True
+
+#     return False
+
+
 
 # unit
-@pytest.paramaterize('input', string.printable)
-def test_input_types(input, color):
+@pytest.mark.parametrize('input', string.ascii_letters)
+def test_input_types(color, input):
     """All letters should pass, all symbols should not"""
 
-    print('input is: ', input)
     rgb = color.collect_rgb(input)
-    print(rgb)
-
-    assert isinstance(tuple[int, int, int], rgb)
-    assert list(rgb) < [255, 255, 255]
+    assert isinstance(rgb, dict)
+    assert list(rgb.values()) <= [255, 255, 255]
