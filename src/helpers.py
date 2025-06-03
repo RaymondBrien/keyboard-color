8f3c043af7abcdf37e26381a6a3a2b1fa6ffc7f6
RaymondBrien <raytbrien@gmail.com>
2025-06-02 20:26:38 -0600

    Remove unused function

--- src/helpers.py
+++ src/helpers.py
@@ -2,10 +2,9 @@
 def within_range(lower, input, upper) -> bool:
     return lower <= input <= upper
 
-def under_25(input) -> bool:
-    """less then or equal to 25, returned as bool"""
-    return input <= 25
-
 def under_255(input):
-    """Max rgb value is 255, Returns bool, less than or equal to 255"""
+    """
+    Max rgb value is 255.
+    Returns bool, less than or equal to 255
+    """
     return input <= 255
