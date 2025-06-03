bc453da8e9ca144ce12835c8bcf667e114b8a7ec
RaymondBrien <raytbrien@gmail.com>
2025-06-02 20:37:53 -0600

    Bug where G values from Q are negative. Validate and fix

--- src/helpers.py
+++ src/helpers.py
@@ -2,9 +2,8 @@
 def within_range(lower, input, upper) -> bool:
     return lower <= input <= upper
 
-def under_255(input):
+def under_255(input) -> bool:
     """
     Max rgb value is 255.
-    Returns bool, less than or equal to 255
     """
-    return input <= 255
+    return 0 <= input <= 255
