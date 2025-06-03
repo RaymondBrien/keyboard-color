f665e886988c450ef0edcaefecd80f20041097fd
RaymondBrien <raytbrien@gmail.com>
2025-06-02 20:38:02 -0600

    Remove defunct function

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -9,13 +9,6 @@ def test_within_range():
     with pytest.raises(TypeError):
         helpers.within_range(0, "a", 10)
 
-def test_under_25():
-    assert helpers.under_25(5) == True
-    assert helpers.under_25(25) == True
-    assert helpers.under_25(26) == False
-    with pytest.raises(TypeError):
-        helpers.under_25("a")
-
 def test_under_255():
     assert helpers.under_255(5) == True
     assert helpers.under_255(255) == True
